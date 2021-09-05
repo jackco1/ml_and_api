e94eba5e48d274790595cf4653dab9c89803f6df
jackco1 <50257114+jackco1@users.noreply.github.com>
2021-09-05 22:06:45 +0800

    ck

--- api.py
+++ api.py
@@ -4,13 +4,16 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+#default page, probs wont make it to production but its just useful
+#for testing
 @app.route('/', methods=['GET'])
 def serve_index():
     return render_template('./index.html')
 
+# ideally should be able to send a bunch of images here at once
 @app.route('/images', methods=['POST'])
 def detect():
-    return jsonify(annotateImages())
+    return annotateImages()
 
 #@app.route('/retrain', methods=['POST'])
 #def fine_tune():
@@ -18,4 +21,4 @@ def detect():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- yolov3/yolo_detection_images.py
+++ yolov3/yolo_detection_images.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import json
 
 def annotateImages():
     confidenceThreshold = 0.8 #confidence threshold of boxes to pick up
@@ -16,62 +17,61 @@ def annotateImages():
 
     net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 
-    imageFiles = ['./images/cat.jpg', './images/dog.jpg', './images.cat1.png']
+    image = cv2.imread('images/dog.jpg')
+    (H, W) = image.shape[:2]
 
-    for imagePath in imageFiles:
-        image = cv2.imread(imagePath)
-        (H, W) = image.shape[:2]
+    #Determine output layer names
+    layerName = net.getLayerNames()
+    layerName = [layerName[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-        #Determine output layer names
-        layerName = net.getLayerNames()
-        layerName = [layerName[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB = True, crop = False)
+    net.setInput(blob)
+    layersOutputs = net.forward(layerName)
 
-        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB = True, crop = False)
-        net.setInput(blob)
-        layersOutputs = net.forward(layerName)
+    boxes = []
+    confidences = []
+    classIDs = []
 
-        boxes = []
-        confidences = []
-        classIDs = []
+    for output in layersOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > confidenceThreshold:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY,  width, height) = box.astype('int')
+                x = int(centerX - (width/2))
+                y = int(centerY - (height/2))
 
-        for output in layersOutputs:
-            for detection in output:
-                scores = detection[5:]
-                classID = np.argmax(scores)
-                confidence = scores[classID]
-                if confidence > confidenceThreshold:
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY,  width, height) = box.astype('int')
-                    x = int(centerX - (width/2))
-                    y = int(centerY - (height/2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
 
-                    boxes.append([x, y, int(width), int(height)])
-                    confidences.append(float(confidence))
-                    classIDs.append(classID)
+    #Apply Non Maxima Suppression
+    detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
 
-        #Apply Non Maxima Suppression
-        detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
+    #for bounding box json output
+    outputs = {}
 
-        outputs = {}
-
-        #ensure detections exist
-        if len(detectionNMS) > 0:
-            outputs['detections'] = {}
-            outputs['detections']['image'] = {}
-            outputs['detections']['image']['annotations'] = []
-            num = 0
-            for i in detectionNMS.flatten():
-                outputs['detections']['image'] = 'image ' + str(num)
-                annotation = {}
-                annotation['class'] = labels[classIDs[i]] #label for class id
-                annotation['cf'] = confidences[i] #confidence rating
-                annotation['x'] = boxes[i][0] #x coordinate (top left)
-                annotation['y'] = boxes[i][1] #y coordinate (top left)
-                annotation['width'] = boxes[i][2] #width
-                annotation['height'] = boxes[i][3] #height
-                outputs['detections']['image']['annotations'].append(annotation)
-                num = num + 1
-        else:
-            outputs['detections']['image'] = 'No detections'
-
-        return outputs
+    #ensure detections exist
+    if len(detectionNMS) > 0:
+        outputs["image"] = {}
+        outputs["image"]["annotations"] = []
+        for i in detectionNMS.flatten():
+            detection = {}
+            detection["label"] = labels[classIDs[i]]
+            detection["cf"] = confidences[i]
+            detection["x"] = boxes[i][0]
+            detection["y"] = boxes[i][1]
+            detection["width"] = boxes[i][2]
+            detection["height"] = boxes[i][3]
+            outputs["image"]["annotations"].append(detection)
+            #print(labels[classIDs[i]]) #label for class id
+            #print(confidences[i]) #confidence rating
+            #print(boxes[i][0]) #x coordinate (top left)
+            #print(boxes[i][1]) #y coordinate (top left)
+            #print(boxes[i][2]) #width
+            #print(boxes[i][3]) #height
+    else:
+        outputs["image"] = "No detections found"
+    return json.dumps(outputs)
